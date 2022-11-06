72fcf9df6799e273a0a7c4f2242c7b96e9a2e1cc
ez4gotit <mdudinov@gmail.com>
2022-11-06 23:47:34 +0600

    1
    1

--- pohuzhe/Assets/Scripts/StatisticsController.cs
+++ pohuzhe/Assets/Scripts/StatisticsController.cs
@@ -9,6 +9,7 @@ public class StatisticsController : MonoBehaviour
     public float experienceCollected { get; private set; } = 0;
     public float playerLevel { get; private set; } = 0;
     public float playerScore { get; private set; } = 0;
+    public GameObject upgradeMenu;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +19,11 @@ public class StatisticsController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(experienceCollected >0 && experienceCollected%10 ==0)
+        {
+            playerLevel++;
+            upgradeMenu.SetActive(true) ;
+        }
     }
 
 
